9c4502b282e33d2ebcdf2c3bf1000619608d530b
elbaqa <beqa488@gmail.com>
2023-06-07 06:30:04 +0400

    changes

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const topnav = document.getElementById('top-nav');
 const menu = document.getElementById("menu");
 const clos = document.getElementById("close");
-
-function navbar(){
-    if(topnav.style.visibility == "hidden"){
-        topnav.style.visibility = "visible";
-        menu.style.display = "none";
-        clos.style.display = "block";
-    } else{
-        topnav.style.visibility = "hidden";
-        menu.style.display = "block";
-        clos.style.display = "none";
-    }
+var x = window.matchMedia("(min-width: 768px)")
+if(x.matches){
+    topnav.style.visibility = "visible";
 }
+function navbar(){
+
+        if(topnav.style.visibility == "hidden"){
+            topnav.style.visibility = "visible";
+            menu.style.display = "none";
+            clos.style.display = "block";
+        } else{
+            topnav.style.visibility = "hidden";
+            menu.style.display = "block";
+            clos.style.display = "none";
+        }
+} 
+    
+
